ed45ed0a5a817f3bd18fdb0b9bad3b9f9314168a
fahrulmr <fahrulmr@gmail.com>
2021-02-17 11:00:03 +0700

    17 feb demo
    - remove light
    - remove master layout
    - add machine status all 3D

---
+++ Assets/script/CMM_MouseOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMM_MouseOver : MonoBehaviour
+{
+    public GameObject cmmstat;
+
+    public void Start()
+    {
+        cmmstat.SetActive(false);
+    } 
+
+    public void OnMouseOver()
+    {
+        cmmstat.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        cmmstat.SetActive(false);
+    }
+}
